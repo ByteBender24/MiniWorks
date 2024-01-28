3557e63e49f6d0932bcdf865fa5af2bc3ce4c2b1
ByteBender24 <harishrajselva@gmail.com>
2024-01-28 13:38:20 +0530

    added gui elements - tkinter

--- youtube_video_downloader/Youtube_video_downloader.py
+++ youtube_video_downloader/Youtube_video_downloader.py
@@ -1,12 +1,14 @@
 from pytube import YouTube
 import tkinter as tk
 from tkinter import filedialog
+from gui import window as gui_window
+from gui import url_entry, format_entry
 
 
-def download_video(url, save_path):
+def download_video(url, save_path, file_ext="mp4"):
     try:
         yt = YouTube(url)
-        streams = yt.streams.filter(progressive=True, file_extension="mp4")
+        streams = yt.streams.filter(progressive=True, file_extension=file_ext)
         highest_res_stream = streams.get_highest_resolution()
         highest_res_stream.download(output_path=save_path)
         print("Video downloaded successfully!")
@@ -18,19 +20,24 @@ def open_file_dialog():
     folder = filedialog.askdirectory()
     if folder:
         print(f"Selected folder: {folder}")
-
     return folder
 
 
+def run_gui_and_download():
+    try:
+        gui_window.mainloop()  # Run the GUI window
+        save_dir = open_file_dialog()
+
+        if save_dir:
+            print("Started download...")
+            print(url_entry, format_entry)
+            download_video(url_entry,
+                           save_dir, format_entry)
+        else:
+            print("Enter correct file path")
+    except:
+        print("Unexpected Error occured....Please Try Again")
+
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()
-
-    video_url = input("Please enter a YouTube url: ")
-    save_dir = open_file_dialog()
-
-    if save_dir:
-        print("Started download...")
-        download_video(video_url, save_dir)
-    else:
-        print("Invalid save location.")
+    run_gui_and_download()
