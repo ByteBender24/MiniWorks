ae88b8a1b06cb590842d2d30c796c96143dd027c
ByteBender24 <harishrajselva@gmail.com>
2024-02-13 23:11:13 +0530

    password_manager_skeleton

---
+++ Password_manager/password_manager.py
@@ -0,0 +1,24 @@
+pwd = input("Enter master password: ")
+
+
+def view():
+    pass
+
+
+def add():
+    pass
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
